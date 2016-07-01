094b713b636419144635b01e632ec7a9912609e4
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-07-01 12:11:02 +0300

    Lesson 1

---
+++ CSharp_lesson_1/CSharp_lesson_1/DialogManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_lesson_1
+{
+    class DialogManager
+    {
+        // Private fields
+        int choise;
+        string ans;
+
+        // Public properties
+        // getter for private 'choise' field
+        public int Choise
+        {
+            get { return choise; }
+        }
+
+        // Public methods
+        public void Start()
+        {
+            Console.WriteLine("\t=======================");
+            Console.WriteLine("\tMy first program on C#");
+            Console.WriteLine("\t=======================");
+        }
+
+        public void Finish()
+        {
+            Console.WriteLine("Bye :)");
+        }
+
+        public bool AllowContinue()
+        {
+            Console.WriteLine("> Continie? (y/n) - ");
+            ans = Console.ReadLine();
+
+            return (ans == "y");
+        }
+
+        public void DisplayMenu()
+        {
+            Console.WriteLine("<1> - solve the equation");
+            Console.WriteLine("<2> - solve the new equation");
+            Console.WriteLine("<3> - exit");
+            Console.WriteLine("> Your choise? - ");
+            try
+            {
+                choise = Convert.ToInt32(Console.ReadLine());
+            }
+            catch(Exception err)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("> Mudak youpt! : {0}", err.Message);
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+        }
+    }
+}
---
+++ CSharp_lesson_1/CSharp_lesson_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DialogManager dm = new DialogManager();
+
+            dm.Start();
+            do
+            {
+                dm.DisplayMenu();
+                switch(dm.Choise)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                }
+            } while(dm.AllowContinue());
+            dm.Finish();
+        }
+    }
+}
