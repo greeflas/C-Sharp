e7d044c4345b2a08396fdb0500a0bee8061a2322
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-07-04 10:27:10 +0300

    Lesson 2: second example

--- CSharp_lesson_2/CSharp_lesson_2/Program.cs
+++ CSharp_lesson_2/CSharp_lesson_2/Program.cs
@@ -6,7 +6,7 @@ namespace CSharp_lesson_2
     {
         static void Main(string[] args)
         {
-            int a = 0, b = 0, c = 0;
+            /*int a = 0, b = 0, c = 0;
 
             // Input a
             Console.WriteLine("Input a");
@@ -75,7 +75,49 @@ namespace CSharp_lesson_2
             if (f1 && f2 && f3)
                 Console.WriteLine("True!");
             else
-                Console.WriteLine("False!");
-          }
+                Console.WriteLine("False!");*/
+
+            int month = 0;
+
+            Console.WriteLine("Inpuit the month (1-12)");
+            Console.Write("> ");
+            try
+            {
+                month = Convert.ToInt16(Console.ReadLine());
+            }
+            catch(Exception err)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine(err.Message);
+                Console.ResetColor();
+            }
+
+            Console.Write("> ");
+            switch (month)
+            {
+                case 1:
+                case 2:
+                    Console.WriteLine("Winter");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Spring");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Summer");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Authumn");
+                    break;
+                case 12:
+                    Console.WriteLine("Winter");
+                    break;
+            }
+        }
     }
 }
