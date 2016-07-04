9d8524c0377bbb664e64cdd20515273fb5fdaaa5
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-07-04 11:03:27 +0300

    Lesson 2: object Random, formatting

---
+++ CSharp_lesson_2.2/CSharp_lesson_2.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CSharp_lesson_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int num = r.Next(0, 10);
+                if(num % 2 == 0 && num != 0)
+                {
+                    Console.Write("> ");
+                    Console.WriteLine(num);
+                }
+            }
+
+            Console.WriteLine("\tPythagoras table of multiply\n");
+            for (int i = 1; i < 10; i++ )
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    if (i * j != 1)
+                        Console.Write("{0, 4}", i * j);
+                    else
+                        Console.Write("{0, 4}", " ");
+                }
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine();
+        }
+    }
+}
