6d4c83339ac8a2f3e070e6422d4221b296bc631f
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-08-17 11:48:38 +0300

    Structures and enums practic

---
+++ CSharp_lesson_9_practic/CSharp_lesson_9_practic/Article.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharp_lesson_9_practic
+{
+    enum ArticleType : uint
+    {
+        Photocameras = 1,
+        Videocameras
+    }
+
+    struct Article
+    {
+        public uint id;
+        public string name;
+        public double price;
+        public ArticleType type;
+
+        public Article(uint id, string name, double price, ArticleType type)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.type = type;
+        }
+
+        public override string ToString()
+        {
+            string GoodsType = Enum.GetName(typeof(ArticleType), type);
+
+            return String.Format("ID: {0} \nName: {1} \nPrice: {2} \nType: {3}\n",
+                id, name, price, GoodsType);
+        }
+    }
+}
---
+++ CSharp_lesson_9_practic/CSharp_lesson_9_practic/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharp_lesson_9_practic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Article a = new Article(1, "Photocamera", 20000.99, ArticleType.Photocameras);
+            Console.WriteLine(a.ToString());
+        }
+    }
+}
