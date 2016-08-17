1e5562e5a989b4b38b5c2a1bcd5ed4416c067835
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-08-17 10:17:29 +0300

    Stuctures

---
+++ CSharp_lesson_9/CSharp_lesson_9/Data.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharp_lesson_9
+{
+    struct Data
+    {
+        public int a;
+        public long b;
+        public double c;
+        public bool d;
+        public string e;
+
+        public int A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+
+        public Data(int a, long b, double c, bool d, string e)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+            this.e = e;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("A: {0} \nB: {1} \nC: {2} \nD: {3} \nE: {4}\n",
+                a, b, c, d, e);
+        }
+    }
+}
---
+++ CSharp_lesson_9/CSharp_lesson_9/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Stuctures */
+
+/*
+Difference
+    Class - reference type, heap
+    Struct - value type, stack
+ */
+
+using System;
+using System.Linq;
+
+namespace CSharp_lesson_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data d = new Data(4, 999999999999999999, 123.9000, true, "hello");
+            Console.WriteLine(d.ToString());
+
+            Data test = d;
+            test.A = 6;
+
+            Console.WriteLine(d.ToString());
+            Console.WriteLine(test.ToString());
+        }
+    }
+}
