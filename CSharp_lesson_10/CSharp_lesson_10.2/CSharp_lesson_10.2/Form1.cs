ca54edb525f4de276d2ea57482a923ccd9dba017
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-08-22 11:54:14 +0300

    Events in Windows Forms

---
+++ CSharp_lesson_10/CSharp_lesson_10.2/CSharp_lesson_10.2/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CSharp_lesson_10._2
+{
+    public partial class Form1 : Form
+    {
+        enum Goods
+        {
+            Phones,
+            Tablets,
+            Notebooks
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = (int)Goods.Phones;
+
+            Goods g = (Goods)comboBox1.SelectedIndex;
+        }
+    }
+}
