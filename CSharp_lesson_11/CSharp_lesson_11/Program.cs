449b9c09504c6a2885fa7364a6b742a3435245dc
Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-08-25 16:39:33 +0300

    Collections: ArrayList

---
+++ CSharp_lesson_11/CSharp_lesson_11/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace CSharp_lesson_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ArrayList(StartCapacity)
+            ArrayList l = new ArrayList(10);
+            Console.WriteLine("\tArrayList");
+            Console.WriteLine("Capacity: {0}", l.Capacity);
+            Console.WriteLine("Count: {0}", l.Count);
+
+            // Add 1 item
+            l.Add(0);
+            Console.WriteLine("\nCapacity: {0}", l.Capacity);
+            Console.WriteLine("Count: {0}", l.Count);
+
+            // Add many item
+            l.AddRange(new int[] { 1, 2, 3, 4, 5 });
+            Console.WriteLine("\nCapacity: {0}", l.Capacity);
+            Console.WriteLine("Count: {0}", l.Count);
+
+            // Capacity x2
+            l.AddRange(new int[] { 6, 7, 8, 9, 10 });
+            Console.WriteLine("\nCapacity: {0}", l.Capacity);
+            Console.WriteLine("Count: {0}", l.Count);
+
+            // Capacity = Count
+            l.TrimToSize();
+            Console.WriteLine("\nCapacity: {0}", l.Capacity);
+            Console.WriteLine("Count: {0}", l.Count);
+
+            l.Add(11);
+            Console.WriteLine("\nCapacity: {0}", l.Capacity);
+            Console.WriteLine("Count: {0}", l.Count);
+
+            Console.WriteLine();
+            foreach (int item in l)
+                Console.Write("{0}, ", item);
+            Console.WriteLine();
+        }
+    }
+}
