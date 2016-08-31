2d077e95f6152323416acbd4ef3ac704301b0837
vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-08-31 11:11:25 +0300

    XML: remove tag

---
+++ CSharp_lesson_14/02-XML/Program.cs
@@ -0,0 +1,32 @@
+﻿/* XML: classes XmlDocument, XmlNode */
+
+using System;
+using System.Xml;
+
+namespace _02_XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string PATH = "../../data/students.xml";
+
+            // create descriptor
+            XmlDocument students = new XmlDocument();
+            // load xml file to descriptor
+            students.Load(PATH);
+
+            // get root tag (<students>)
+            XmlNode root = students.DocumentElement;
+
+            // edit data
+
+            // remove
+            XmlNode student = root.ChildNodes[0];
+            root.RemoveChild(student);
+            students.Save(PATH);
+
+            Console.WriteLine("Deleted!");
+        }
+    }
+}
