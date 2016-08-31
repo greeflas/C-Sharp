d4259e47260ae28dc0a1cd9f3d980c1d0b7700ab
vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-08-31 11:02:45 +0300

    XML: create new tag

--- CSharp_lesson_14/XML/Program.cs
+++ CSharp_lesson_14/XML/Program.cs
@@ -30,6 +30,35 @@ namespace CSharp_lesson_14
                 }
                 Console.WriteLine();
             }
+
+            // create node
+            XmlElement newNode = students.CreateElement("student");
+
+            XmlAttribute id = students.CreateAttribute("id");
+            XmlAttribute fname = students.CreateAttribute("fname");
+            XmlAttribute lname = students.CreateAttribute("lname");
+            XmlAttribute age = students.CreateAttribute("age");
+            XmlAttribute rate = students.CreateAttribute("rate");
+
+            Console.Write("id > ");
+            id.Value = Console.ReadLine();
+            Console.Write("First name > ");
+            fname.Value = Console.ReadLine();
+            Console.Write("Last name > ");
+            lname.Value = Console.ReadLine();
+            Console.Write("Age > ");
+            age.Value = Console.ReadLine();
+            Console.Write("Rate > ");
+            rate.Value = Console.ReadLine();
+
+            newNode.Attributes.Append(id);
+            newNode.Attributes.Append(fname);
+            newNode.Attributes.Append(lname);
+            newNode.Attributes.Append(age);
+            newNode.Attributes.Append(rate);
+
+            root.AppendChild(newNode);
+            students.Save(PATH);
         }
     }
 }
