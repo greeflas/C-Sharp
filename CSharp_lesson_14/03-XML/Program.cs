05bb3f0f0fed4a87b069b5d8f9c98bfbe1cb69e3
vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-08-31 11:26:59 +0300

    XML: edit tag attributes, find node by attribute value

---
+++ CSharp_lesson_14/03-XML/Program.cs
@@ -0,0 +1,42 @@
+﻿/* XML: classes XmlDocument, XmlNode */
+
+using System;
+using System.Xml;
+
+namespace _03_XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string PATH = "../../data/students.xml";
+
+            // create descriptor
+            XmlDocument students = new XmlDocument();
+            // load xml file to descriptor
+            students.Load(PATH);
+
+            // get root tag (<students>)
+            XmlNode root = students.DocumentElement;
+
+            // search student by last name
+            Console.Write("Last name: ");
+            string studentLname = Console.ReadLine();
+
+            int sID = 0;
+            XmlNodeList nodes = students.GetElementsByTagName("student");
+            foreach(XmlNode node in nodes)
+            {
+                if(node.Attributes[2].Value == studentLname)
+                    break;
+                ++sID;
+            }
+
+            Console.Write("New rate: ");
+            string rate = Console.ReadLine();
+            nodes[sID].Attributes[4].Value = rate;
+
+            students.Save(PATH);
+        }
+    }
+}
