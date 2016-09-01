10f884b020f234c8a4058a8cdf456b641b6e87a2
Vladimir Kuprienko <greeflas@users.noreply.github.com>
2016-09-01 11:52:26 +0300

    Add files via upload

---
+++ foreach.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace @foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 0, -1, 2, -3, 4, -5, 6, -7, 8, -9 };
+
+            Console.WriteLine("foreach");
+            foreach(var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\n\nIEnumerator");
+            IEnumerable enbl = arr;
+            IEnumerator entr = enbl.GetEnumerator();
+
+            entr.Reset();
+            while(entr.MoveNext())
+            {
+                Console.Write(entr.Current + " ");
+            }
+
+            Console.WriteLine("\n");
+        }
+    }
+}
