d53aef2d27bc4448e4d110095912fecbd756dbaf
vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-09-05 09:53:50 +0300

    XML: streams

---
+++ CSharp_lesson_15/XmlStreams/Program.cs
@@ -0,0 +1,53 @@
+﻿/* XML: streams */
+
+using System;
+using System.Xml;
+
+namespace XmlStreams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string PATH = "../../data/data.xml";
+
+            // Open stream
+            XmlTextReader reader = new XmlTextReader(PATH);
+            // Clear from spaces
+            reader.WhitespaceHandling = WhitespaceHandling.All;
+
+            // Read data
+            while(reader.Read())
+            {
+                // node type
+                XmlNodeType type = reader.NodeType;
+                //Console.WriteLine("Node type: {0}", type.ToString());
+
+                // node name
+                string name = reader.Name;
+                //Console.WriteLine("Node name: {0}", name);
+
+                // node value
+                string value = reader.Value;
+                //Console.WriteLine("Node value: {0}", value);
+
+                // attributes count
+                int attrCount = reader.AttributeCount;
+                //Console.WriteLine("Attribute count: {0}", attrCount);
+                //Console.WriteLine();
+
+                // read node attributes
+                if((reader.NodeType == XmlNodeType.Element) && (reader.AttributeCount > 0))
+                {
+                    Console.WriteLine("id: {0}", reader.GetAttribute("id"));
+                    Console.WriteLine("Title: {0}", reader.GetAttribute("title"));
+                    Console.WriteLine("Category: {0}", reader.GetAttribute("category"));
+                    Console.WriteLine();
+                }
+            }
+
+            // close xml stream
+            reader.Close();
+        }
+    }
+}
