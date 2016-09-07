70d40459c7a12d137bea91e0e3d63c4f14a27b9f
vladimir Kuprienko <vldmr.kuprienko@gmail.com>
2016-09-07 10:27:50 +0300

    Serialize/Deserialize class object

---
+++ CSharp_lesson_16/01-Serialize/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace _01_Serialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string PATH = "../../data/student.dat";
+
+            /* Serialize */
+
+            // Create class object
+            Student s = new Student(1, "Vasya", "Pupkin", 10.0);
+
+            // Open file stream
+            FileStream fs = new FileStream(PATH, FileMode.Create, FileAccess.Write);
+
+            // Serialize class object and write it to file
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, s);
+
+            fs.Close();
+
+            /* Deserialize */
+
+            // Open file stream
+            FileStream fs2 = new FileStream(PATH, FileMode.Open, FileAccess.Read);
+
+            BinaryFormatter bf2 = new BinaryFormatter();
+            Student s2 = (Student)bf2.Deserialize(fs2);
+
+            fs2.Close();
+
+            Console.WriteLine(s2.ToString());
+        }
+    }
+}
---
+++ CSharp_lesson_16/01-Serialize/Student.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _01_Serialize
+{
+    [Serializable]
+    class Student
+    {
+        // Private fields
+        uint id;
+        string first_name;
+        string last_name;
+        double rate;
+
+        // Properties
+        public uint Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string First_name
+        {
+            get { return first_name; }
+            set { first_name = value; }
+        }
+
+        public string Last_name
+        {
+            get { return last_name; }
+            set { last_name = value; }
+        }
+
+        public double Rate
+        {
+            get { return rate; }
+            set { rate = value; }
+        }
+
+        // Public methods
+        public Student(uint id, string first_name, string last_name, double rate)
+        {
+            this.id = id;
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.rate = rate;
+        }
+
+        public override string ToString()
+        {
+            return String.Format(
+                "id: {0}"+
+                "\nFirst name: {1}"+
+                "\nLast name: {2}"+
+                "\nRate: {3}",
+
+                id,
+                first_name,
+                last_name,
+                rate
+            );
+        }
+    }
+}
